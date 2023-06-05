57a05840b3737778d690de3f54359cdcc41e1504
Rosanna <rosannamollie@gmail.com>
2023-06-05 16:01:49 -0700

    App.js manufact fetch

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AutomobileList from './ListAutomobiles';
+
+
 
 function App() {
+
+  const [ manufacturers, setManufacturers ] = useState([]);
+
+  async function getManufacturers() {
+    const url = 'http://localhost:8100/api/manufacturers/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json()
+      setManufacturers(data.manufacturers)
+    }
+  }
+
+    useEffect(() => {
+      getManufacturers();
+    }, []);
+
   return (
     <BrowserRouter>
       <Nav />
---
+++ ghi/app/src/ListAutomobiles.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function AutomobileList(props){
+    return ();
+}
+
+export default AutomobileList
