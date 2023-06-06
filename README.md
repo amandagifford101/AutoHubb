a84a6df161e3953e73826d9e6396c4efe2635c77
Rosanna <rosannamollie@gmail.com>
2023-06-06 12:02:00 -0700

    service models

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Team:
 
-* Person 1 - Which microservice?
-* Person 2 - Which microservice?
+* Rosanna Wyatt - Service
+* Amanda Gifford - Sales
 
 ## Design
 
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17)
+    sold = models.BooleanField()
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(max_length=50, default="scheduled")
+    vin = models.CharField(max_length=17)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.PROTECT,
+    )
